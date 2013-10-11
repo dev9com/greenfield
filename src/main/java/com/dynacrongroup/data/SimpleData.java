5bd67454889c8388a8958f1f0af18903ef3d7a1d
Will Iverson <wiverson@Lambda.local>
2013-10-10 17:49:45 -0700

    Simplified for now

--- src/main/java/com/dynacrongroup/data/SimpleData.java
+++ src/main/java/com/dynacrongroup/data/SimpleData.java
@@ -2,16 +2,7 @@ package com.dynacrongroup.data;
 
 public class SimpleData {
 
-    private String hello;
-    private String world;
-
-    public String getWorld() {
-        return world;
-    }
-
-    public void setWorld(String world) {
-        this.world = world;
-    }
+    private String hello = "world";
 
     public String getHello() {
         return hello;
